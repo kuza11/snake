226e807bf4cebe381d817898215a53f58a0ef0f6
unknown <kuba.zvolanek@seznam.cz>
2023-05-28 21:55:48 +0200

    init

---
+++ PC_had/PC_had/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace PC_had
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(407, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PC_had/PC_had/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PC_had
+{
+    public partial class Form1 : Form
+    {
+        int cnt;
+        int step_size_x;
+        int step_size_y;
+        string path = "C:\\Users\\kuza\\source\\repos\\PC_had\\PC_had\\pic\\";
+        PictureBox[] snake_body;
+        PictureBox snake_head = new PictureBox();
+        PictureBox snake_tail = new PictureBox();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            step_size_x = (int)this.Size.Width/30;
+            step_size_y = (int)this.Size.Height/30;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            snake_head.ImageLocation = path + "snake_head.png";
+            snake_head.Size = new Size(30, 38);
+            snake_head.Parent = this;
+            snake_head.Location = new Point(step_size_x*5, step_size_y*5);
+
+            snake_body = new PictureBox[1];
+            snake_body[0] = new PictureBox();
+            snake_body[0].ImageLocation = path + "snake_body.png";
+            snake_body[0].Size = new Size(30, 26);
+            snake_body[0].Parent = this;
+            snake_body[0].Location = new Point(snake_head.Location.X - 30, snake_head.Location.Y + 6);
+
+            snake_tail.ImageLocation = path + "snake_tail.png";
+            snake_tail.Size = new Size(30, 26);
+            snake_tail.Parent = this;
+            snake_tail.Location = new Point(snake_body.Last().Location.X - 30 , snake_body.Last().Location.Y);
+
+            cnt = 6;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            snake_tail.Location = snake_body.Last().Location;
+            for (int i = 0; i < snake_body.Length; i++)
+            {
+                if(i == 0)
+                {
+                    snake_body[i].Location = new Point(snake_head.Location.X, snake_head.Location.Y + 6);
+                }
+                else
+                {
+                    snake_body[i].Location = snake_body[i-1].Location; 
+                }
+            }
+            snake_head.Location = new Point(step_size_x * cnt, step_size_y*5);
+            cnt++;
+        }
+    }
+}
