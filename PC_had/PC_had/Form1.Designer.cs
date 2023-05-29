c098e58ad436c026a430c28368d77e9c2cb18bec
kuza11 <kuza2017@seznam.cz>
2023-05-29 20:02:20 +0200

    prg

--- PC_had/PC_had/Form1.Designer.cs
+++ PC_had/PC_had/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -48,17 +50,39 @@
             this.timer1.Interval = 500;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 342);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -66,6 +90,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- PC_had/PC_had/Form1.cs
+++ PC_had/PC_had/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,10 +16,19 @@ namespace PC_had
         int cnt;
         int step_size_x;
         int step_size_y;
-        string path = "C:\\Users\\kuza\\source\\repos\\snake\\PC_had\\PC_had\\pic\\";
+
+        int direction = 1;
+
+
+        //string path = "C:\\Users\\kuza\\source\\repos\\snake\\PC_had\\PC_had\\pic\\";
+        string path = "C:\\Users\\kuza\\git\\snake\\PC_had\\PC_had\\pic\\";
+
         PictureBox[] snake_body;
+        int[] snake_body_direction = { 1 };
         PictureBox snake_head = new PictureBox();
+        int snake_head_direction = 1;
         PictureBox snake_tail = new PictureBox();
+        int snake_tail_direction = 1;
         public Form1()
         {
             InitializeComponent();
@@ -26,51 +36,115 @@ namespace PC_had
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            step_size_x = (int)this.Size.Width/30;
-            step_size_y = (int)this.Size.Height/30;
+            step_size_x = 30;
+            step_size_y = 30;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             button1.Visible = false;
-            snake_head.ImageLocation = path + "snake_head.png";
-            snake_head.Size = new Size(30, 38);
+            snake_head.Image = Image.FromFile(path + "snake_head.png");
+            snake_head.Size = new Size(30, 30);
             snake_head.Parent = this;
             snake_head.Location = new Point(step_size_x*5, step_size_y*5);
 
+
             snake_body = new PictureBox[1];
             snake_body[0] = new PictureBox();
-            snake_body[0].ImageLocation = path + "snake_body.png";
-            snake_body[0].Size = new Size(30, 26);
+            snake_body[0].Image = Image.FromFile(path + "snake_body.png");
+            snake_body[0].Size = new Size(30, 30);
             snake_body[0].Parent = this;
-            snake_body[0].Location = new Point(snake_head.Location.X - 30, snake_head.Location.Y + 6);
+            snake_body[0].Location = new Point(snake_head.Location.X - 30, snake_head.Location.Y);
 
-            snake_tail.ImageLocation = path + "snake_tail.png";
-            snake_tail.Size = new Size(30, 26);
+            snake_tail.Image = Image.FromFile(path + "snake_tail.png");
+            snake_tail.Size = new Size(30, 30);
             snake_tail.Parent = this;
             snake_tail.Location = new Point(snake_body.Last().Location.X - 30 , snake_body.Last().Location.Y);
 
-            cnt = 6;
+           
             timer1.Enabled = true;
         }
-
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            int t = 0;
             snake_tail.Location = snake_body.Last().Location;
             for (int i = 0; i < snake_body.Length; i++)
             {
                 if(i == 0)
                 {
-                    snake_body[i].Location = new Point(snake_head.Location.X, snake_head.Location.Y + 6);
+                    snake_body[i].Location = snake_head.Location;
+                    if(direction - snake_body_direction[i] == 1 || direction - snake_body_direction[i] == -3)
+                    {
+                        snake_body[i].Image = Image.FromFile(path + "snake_body.png");
+                        snake_body[i].Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    }else if (direction - snake_body_direction[i] == -1 || direction - snake_body_direction[i] == 3)
+                    {
+                        snake_body[i].Image = Image.FromFile(path + "snake_body.png");
+                        snake_body[i].Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    }
                 }
                 else
                 {
                     snake_body[i].Location = snake_body[i-1].Location; 
                 }
             }
-            snake_head.Location = new Point(step_size_x * cnt, step_size_y*5);
-            cnt++;
+            
+            snake_head.Image = Image.FromFile(path + "snake_head.png");
+
+            if (direction - snake_head_direction > 0)
+            {
+                
+                for (int i = direction - snake_head_direction; i > 0; i--)
+                {
+                    
+                    snake_head.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                }
+                
+            }
+            else if(direction - snake_head_direction < 0)
+            {
+                for(int i = direction - snake_head_direction; i < 0; i++)
+                {
+                    snake_head.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                }
+            }
+            label2.Text = Convert.ToString(direction);
+            switch (direction)
+            {
+                case 1:
+                    snake_head.Location = new Point(snake_head.Location.X + step_size_x, snake_head.Location.Y);
+                    break;
+                case 2:
+                    snake_head.Location = new Point(snake_head.Location.X, snake_head.Location.Y - step_size_y);
+                    break;
+                case 3:
+                    snake_head.Location = new Point(snake_head.Location.X - step_size_x, snake_head.Location.Y);
+                    break;
+                case 4:
+                    snake_head.Location = new Point(snake_head.Location.X, snake_head.Location.Y + step_size_y);
+                    break;
+            }
+            
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (Convert.ToString(e.KeyCode)){
+                case "D":
+                    direction = 1;
+                    break;
+                case "W":
+                    direction = 2;
+                    break;
+                case "A":
+                    direction = 3;
+                    break;
+                case "S":
+                    direction = 4;
+                    break;
+            }
+            e.SuppressKeyPress = true;
         }
     }
 }
