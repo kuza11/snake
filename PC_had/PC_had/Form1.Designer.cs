369d7f49d5316c79fca19840ccf10063ad3fb958
kuza11 <kuza2017@seznam.cz>
2023-05-30 14:05:24 +0200

    food

--- PC_had/PC_had/Form1.Designer.cs
+++ PC_had/PC_had/Form1.Designer.cs
@@ -31,13 +31,16 @@
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(407, 274);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 21);
             this.button1.TabIndex = 0;
@@ -47,25 +50,63 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 500;
+            this.timer1.Interval = 250;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(212, 168);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
-            this.label1.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(253, 342);
+            this.label2.BackColor = System.Drawing.Color.Silver;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(282, 232);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(323, 55);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "GAME OVER";
+            this.label2.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 39);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "easy";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 62);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "medium";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 85);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(46, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.Text = "hard";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Press any key to continue.";
+            this.label1.Visible = false;
             // 
             // Form1
             // 
@@ -73,8 +114,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(884, 561);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.KeyPreview = true;
             this.Name = "Form1";
@@ -90,8 +134,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- PC_had/PC_had/Form1.cs
+++ PC_had/PC_had/Form1.cs
@@ -13,18 +13,26 @@ namespace PC_had
 {
     public partial class Form1 : Form
     {
-        int cnt;
+        bool resize = false;
+        Random random = new Random();
+
+        Point[] old_pos = new Point[2];
+
+        int next = 0;
+        bool[] occupancy = new bool[10];
+        
+
         int step_size_x;
         int step_size_y;
 
-        int direction = 1;
+        int direction;
 
 
         //string path = "C:\\Users\\kuza\\source\\repos\\snake\\PC_had\\PC_had\\pic\\";
         string path = "C:\\Users\\kuza\\git\\snake\\PC_had\\PC_had\\pic\\";
 
+        PictureBox[] food = new PictureBox[10];
         PictureBox[] snake_body;
-        int[] snake_body_direction = { 1 };
         PictureBox snake_head = new PictureBox();
         int snake_head_direction = 1;
         PictureBox snake_tail = new PictureBox();
@@ -42,11 +50,26 @@ namespace PC_had
 
         private void button1_Click(object sender, EventArgs e)
         {
+            direction = 1;
+            for(int i = 0; i < food.Length; i++)
+            {
+                food[i] = new PictureBox();
+                food[i].Size = new Size(30, 30);
+                food[i].Parent = this;
+                food[i].SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            for(int i = 0; i < occupancy.Length; i++)
+            {
+                occupancy[i] = false;
+            }
+
+
             button1.Visible = false;
             snake_head.Image = Image.FromFile(path + "snake_head.png");
             snake_head.Size = new Size(30, 30);
             snake_head.Parent = this;
             snake_head.Location = new Point(step_size_x*5, step_size_y*5);
+            snake_head.Visible = true;
 
 
             snake_body = new PictureBox[1];
@@ -56,39 +79,107 @@ namespace PC_had
             snake_body[0].Parent = this;
             snake_body[0].Location = new Point(snake_head.Location.X - 30, snake_head.Location.Y);
 
+
             snake_tail.Image = Image.FromFile(path + "snake_tail.png");
             snake_tail.Size = new Size(30, 30);
             snake_tail.Parent = this;
             snake_tail.Location = new Point(snake_body.Last().Location.X - 30 , snake_body.Last().Location.Y);
+            snake_tail.Visible = true;
 
-           
+            if (radioButton1.Checked)
+            {
+                timer1.Interval = 500;
+            }
+            else if (radioButton2.Checked)
+            {
+                timer1.Interval = 250;
+            }
+            else
+            {
+                timer1.Interval = 150;
+            }
+
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+            radioButton3.Visible = false;
+
             timer1.Enabled = true;
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            int t = 0;
-            snake_tail.Location = snake_body.Last().Location;
-            for (int i = 0; i < snake_body.Length; i++)
+            int free = -1;
+            bool fit = false;
+            next++;
+            if(next > 6)
             {
-                if(i == 0)
+                for(int i = 0; i < occupancy.Length; i++)
                 {
-                    snake_body[i].Location = snake_head.Location;
-                    if(direction - snake_body_direction[i] == 1 || direction - snake_body_direction[i] == -3)
+                    if (!occupancy[i])
                     {
-                        snake_body[i].Image = Image.FromFile(path + "snake_body.png");
-                        snake_body[i].Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
-                    }else if (direction - snake_body_direction[i] == -1 || direction - snake_body_direction[i] == 3)
-                    {
-                        snake_body[i].Image = Image.FromFile(path + "snake_body.png");
-                        snake_body[i].Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        free = i;
+                        break;
                     }
                 }
-                else
+                next = 0;
+                if (free >= 0)
                 {
-                    snake_body[i].Location = snake_body[i-1].Location; 
+                    food[free].Image = Image.FromFile(path + "PCparts\\image_(" + random.Next(1, 11) + ").png");
+                    food[free].Location = new Point(30 * random.Next(this.Width / 30 - 1), 30 * random.Next(this.Height / 30 - 1));
+                    for(int i = 0;i < snake_body.Length; i++)
+                    {
+                        if (food[free].Location == snake_body[i].Location)
+                        {
+                            fit = true;
+                        }
+                    }
+                    while (food[free].Location == snake_head.Location || food[free].Location == snake_tail.Location || fit)
+                    {
+                        fit = false;
+                        food[free].Location = new Point(30 * random.Next(this.Width / 30 - 1), 30 * random.Next(this.Height / 30 - 1));
+                        for (int i = 0; i < snake_body.Length; i++)
+                        {
+                            if (food[free].Location == snake_body[i].Location)
+                            {
+                                fit = true;
+                            }
+                        }
+                    }
+
+                    occupancy[free] = true;
                 }
             }
-            
+
+
+
+            if (resize)
+            {
+                resize = false;
+                Array.Reverse(snake_body);
+                Array.Resize(ref snake_body, snake_body.Length+1);
+                Array.Reverse(snake_body);
+                snake_body[0] = new PictureBox();
+                snake_body[0].Image = Image.FromFile(path + "snake_body.png");
+                snake_body[0].Size = new Size(30, 30);
+                snake_body[0].Parent = this;
+                snake_body[0].Location = snake_head.Location;
+            } else {
+
+
+                snake_tail.Location = snake_body.Last().Location;
+                for (int i = 0; i < snake_body.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        old_pos[0] = snake_body[i].Location;
+                        snake_body[i].Location = snake_head.Location;
+                    }
+                    else
+                    {
+                        old_pos[i % 2] = snake_body[i].Location;
+                        snake_body[i].Location = old_pos[i % 2 == 1 ? 0 : 1];
+                    }
+                }
+            }
             snake_head.Image = Image.FromFile(path + "snake_head.png");
 
             if (direction - snake_head_direction > 0)
@@ -108,7 +199,6 @@ namespace PC_had
                     snake_head.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
                 }
             }
-            label2.Text = Convert.ToString(direction);
             switch (direction)
             {
                 case 1:
@@ -124,12 +214,60 @@ namespace PC_had
                     snake_head.Location = new Point(snake_head.Location.X, snake_head.Location.Y + step_size_y);
                     break;
             }
-            
+            snake_tail.Image = Image.FromFile(path + "snake_tail.png");
+            if (snake_body.Last().Location.X < snake_tail.Location.X)
+            {
+                snake_tail.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            }
+            else if (snake_body.Last().Location.Y > snake_tail.Location.Y)
+            {
+                snake_tail.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            }
+            else if (snake_body.Last().Location.Y < snake_tail.Location.Y)
+            {
+                snake_tail.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            }
+
+            if(snake_head.Location.X < 0 || snake_head.Location.X + 30 > this.Width || snake_head.Location.Y < 0 || snake_head.Location.Y + 31 > this.Height || snake_head.Location == snake_tail.Location)
+            {
+                timer1.Enabled = false;
+                label2.Visible = true;
+                label1.Visible = true;
+            }
+            for(int i = 0; i < snake_body.Length; i++)
+            {
+                if (snake_body[i].Location == snake_head.Location)
+                {
+                    timer1.Enabled = false;
+                    label2.Visible = true;
+                    label1.Visible = true;
+                }
+            }
 
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+            if(label2.Visible == true)
+            {
+                label2.Visible = false;
+                label1.Visible = false;
+                snake_head.Visible = false;
+                snake_tail.Visible = false;
+                foreach(PictureBox seg in snake_body)
+                {
+                    seg.Visible = false;
+                }
+                foreach(PictureBox item in food)
+                {
+
+                    item.Visible = false;
+                }
+                button1.Visible = true;
+                radioButton1.Visible = true;
+                radioButton2.Visible = true;
+                radioButton3.Visible = true;
+            }
             switch (Convert.ToString(e.KeyCode)){
                 case "D":
                     direction = 1;
@@ -142,6 +280,9 @@ namespace PC_had
                     break;
                 case "S":
                     direction = 4;
+                    break;
+                case "L":
+                    resize = true;
                     break;
             }
             e.SuppressKeyPress = true;
